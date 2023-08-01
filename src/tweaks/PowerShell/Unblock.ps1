711fb0390b831d3a840a7d85ebc220045eb0ce71
hajkusi <hajkusipl@gmail.com>
2023-08-01 18:51:44 +0200

    Commit

--- src/tweaks/PowerShell/Unblock.ps1
+++
@@ -1,7 +0,0 @@
-Unblock-File -Path C:\Users\hajku\Pulpit\gaming\gaming\1.PowerShell\WindowsToolbox-1.0.4\main.ps1
-dir C:\Users\hajku\Pulpit\gaming\gaming\1.PowerShell\WindowsToolbox-1.0.4\ -File -Recurse | Unblock-File
----------------^
----------------|
----------------|
----------------|
------------Twoja nazwa użytkownika
