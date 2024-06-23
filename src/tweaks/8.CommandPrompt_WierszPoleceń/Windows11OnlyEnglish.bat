c0e46eec776f058c900fef35b5772aeab87b94f8
hajkusi <138481906+hajkusi@users.noreply.github.com>
2024-06-23 14:53:40 +0200

    Add files via upload

---
+++ src/tweaks/8.CommandPrompt_WierszPoleceń/Windows11OnlyEnglish.bat
@@ -0,0 +1,44 @@
+@echo off
+Set /a time=6
+cls
+goto CheckPermissions
+
+
+:CheckPermissions
+  Net File 
+   if "%errorlevel%"=="0" ( goto W11Only ) else ( goto UACPrompt )
+
+:UACPrompt
+cls
+powershell -NoProfile -Command start -verb runas "'%~s0'" && exit /b
+goto Timer
+
+:W11Only
+cls
+netsh int tcp set supplemental Internet CongestionProvider=BBR2
+netsh int tcp set supplemental Custom CongestionProvider=BBR2
+netsh int tcp set supplemental InternetCustom CongestionProvider=BBR2
+netsh int tcp set supplemental Datacenter CongestionProvider=BBR2
+netsh int tcp set supplemental Compat CongestionProvider=BBR2
+netsh int tcp set supplemental Template=Internet CongestionProvider=BBR2
+netsh int tcp set supplemental Template=Datacenter CongestionProvider=BBR2
+netsh int tcp set supplemental Template=Compat CongestionProvider=BBR2
+netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=BBR2
+netsh int tcp set supplemental Template=InternetCustom CongestionProvider=BBR2
+Echo BBR2 ( Best Congestion Provider Is Applied! )
+Timeout /t 2 /nobreak  > Nul
+goto Timer
+
+
+:Timer
+cls
+cls
+Set /a time=%time%-1
+If %time% == 0 goto Done
+Call Echo %~n0%~x0 Turns Off Itself In %time% Seconds
+Ping localhost -n 2 >nul
+goto Timer
+
+
+:Done
+Exit /b
