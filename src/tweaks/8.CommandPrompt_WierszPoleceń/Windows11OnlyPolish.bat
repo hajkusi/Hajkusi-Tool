f0bfd73882fc720b975abed37f390a9b7f29e2f9
hajkusi <138481906+hajkusi@users.noreply.github.com>
2024-06-23 14:44:11 +0200

    Update and rename Windows11Only.bat to Windows11OnlyPolish.bat

--- src/tweaks/8.CommandPrompt_WierszPoleceń/Windows11OnlyPolish.bat
+++ src/tweaks/8.CommandPrompt_WierszPoleceń/Windows11OnlyPolish.bat
@@ -1,4 +1,19 @@
 @echo off
+set /a time=5
+cls
+goto CheckPermissions
+
+
+:CheckPermissions
+  Net File 
+   if "%errorlevel%"=="0" ( goto W11Only ) else ( goto UACPrompt )
+
+:UACPrompt
+cls
+powershell -NoProfile -Command start -verb runas "'%~s0'" && exit /b
+goto Timer
+
+:W11Only
 netsh int tcp set supplemental Internet CongestionProvider=BBR2
 netsh int tcp set supplemental Custom CongestionProvider=BBR2
 netsh int tcp set supplemental InternetCustom CongestionProvider=BBR2
@@ -9,3 +24,19 @@ netsh int tcp set supplemental Template=Datacenter CongestionProvider=BBR2
 netsh int tcp set supplemental Template=Compat CongestionProvider=BBR2
 netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=BBR2
 netsh int tcp set supplemental Template=InternetCustom CongestionProvider=BBR2
+Timeout /t 2 /nobreak
+goto Timer
+
+
+:Timer
+cls
+cls
+set /a time=%time%-1
+if %time% == 0 goto :done
+call echo %~n0%~x0 wylaczy sie za %time% sekund
+ping localhost -n 2 >nul
+goto Timer
+
+
+:Done
+Exit /b
