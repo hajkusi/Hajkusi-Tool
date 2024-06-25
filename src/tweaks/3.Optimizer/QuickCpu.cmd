bb4bc9bc09f93505f83560d07d9bd4b1280bc079
hajkusi <138481906+hajkusi@users.noreply.github.com>
2024-06-25 20:55:03 +0200

    Add files via upload

---
+++ src/tweaks/3.Optimizer/QuickCpu.cmd
@@ -0,0 +1,4 @@
+@Echo off
+curl -L -# -o "%SYSTEMDRIVE%/Gaming-Pack/Resources/QuickCpuSetup64.zip" https://coderbag.com/assets/downloads/cpm/currentversion/QuickCpuSetup64.zip
+start %SYSTEMDRIVE%/Gaming-Pack/Resources/QuickCpuSetup64.zip
+exit /b
